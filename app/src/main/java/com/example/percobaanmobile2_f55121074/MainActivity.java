107f5de6015ce2c1c48295dad3954df5a3b09341
YunitaAnggeraini F55121070 <yunitaanggeraini204@gmail.com>
2022-10-20 17:53:00 -0800

    Initial commit

---
+++ app/src/main/java/com/example/percobaanmobile2_f55121074/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.percobaanmobile2_f55121074;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] dataName;
+    private String[] dataDescription;
+    private TypedArray dataPhoto;
+    private HeroAdapter adapter;
+    private ArrayList<Hero> heroes;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        adapter = new HeroAdapter(this);
+        ListView listView = findViewById(R.id.lv_list);
+        List<String> list = new ArrayList<>();
+        list.add("Valda_074");
+        ArrayAdapter arrayAdapter =  new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+                if (position==0){
+                    startActivity(new Intent(MainActivity.this, detail_F55121074.class));
+                }
+            }
+        });
+        listView.setAdapter(adapter);
+        prepare();
+        addItem();
+    }
+    private void addItem () {
+        heroes = new ArrayList<>();
+        for (int i = 0; i < dataName.length; i++) {
+            Hero hero = new Hero();
+            hero.setPhoto(dataPhoto.getResourceId(i, -1));
+            hero.setName(dataName[i]);
+            hero.setDescription(dataDescription[i]);
+            heroes.add(hero);
+        }
+        adapter.setHeroes(heroes);
+    }
+    private void prepare() {
+        dataName = getResources().getStringArray(R.array.data_name);
+        dataDescription = getResources().getStringArray(R.array.data_description);
+        dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
+    }
+
+}
---
+++ app/src/main/java/com/example/percobaanmobile2_f55121074/detail_F55121074.java
@@ -0,0 +1,14 @@
+package com.example.percobaanmobile2_f55121074;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detail_F55121074 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_f55121074);
+    }
+}
